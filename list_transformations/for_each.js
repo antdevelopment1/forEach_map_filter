c706e85df2d538733a9a7154fda199209fcd6ef3
April Copes <antdevelopment1@gmail.com>
2018-10-09 16:07:26 -0400

    More examples on for each

--- list_transformations/for_each.js
+++ list_transformations/for_each.js
@@ -1,25 +1,25 @@
-// function CreateSuspectObjects(name) {
-//     return {
-//         name: name,
-//         color: name.split(' ')[1],
-//         speak() {
-//           console.log('My name is ', name);
-//         }
-//     };
-// };
+function CreateSuspectObjects(name) {
+    return {
+        name: name,
+        color: name.split(' ')[1],
+        speak() {
+          console.log('My name is ', name);
+        }
+    };
+};
 
-// var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr White'];
-// var suspectsList = [];
+var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr White'];
+var suspectsList = [];
 
-// for (var i = 0; i < suspects.length; i++) {
+for (var i = 0; i < suspects.length; i++) {
     
-//     suspectsList.push(CreateSuspectObjects(suspects[i]));
-// }
+    suspectsList.push(CreateSuspectObjects(suspects[i]));
+}
 
 
-// console.log(CreateSuspectObjects("Miss Scarlet"));
-// console.log(CreateSuspectObjects("Colonel Mustard"));
-// console.log(CreateSuspectObjects("Mr White"));
+console.log(CreateSuspectObjects("Miss Scarlet"));
+console.log(CreateSuspectObjects("Colonel Mustard"));
+console.log(CreateSuspectObjects("Mr White"));
 
 function CreateSuspectObjects(name) {
     return {
@@ -32,6 +32,29 @@ function CreateSuspectObjects(name) {
 var suspects = ['Miss Scarlet', 'Colonel Mustard', 'Mr White'];
 
 var suspectsList = [];
+
+_.each(suspects, function(name) {
+  console.log(CreateSuspectObjects(name));
+})
+
+const _ = {};
+_.each = function(list, callback) {
+  if (Array.isArray(list)) {
+      for (var i = 0; i < list.length; i++) {
+        callback(list[i], i, list);
+      }
+  } else {
+
+  }
+}
+_.each(["greg", "stan", "jodi"], function(name, current, list) {
+  if (list[current + 1]) {
+      console.log(`${name} is younger than ${list[current + 1]}`);
+  } else {
+      console.log(`${name}, is the oldest!`);
+  }
+});
+
 
 _.each(suspects, function(name) {
     let suspectObj = CreateSuspectObjects(name);
